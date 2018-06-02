f52f5c21dd4f40b51d2b5eb650d726f34f1f922b
Ken Lowrie <klowrie@gmail.com>
2018-06-02 07:38:14 -0500

    Added new submodules.

--- avs/__init__.py
+++ avs/__init__.py
@@ -2,9 +2,12 @@
 
 from .file import *
 from .link import *
+from .line import *
+from .regex import *
 from .variable import *
 from .bookmark import *
 from .exception import *
+from .htmlformat import *
 
 if __name__ == '__main__':
     print("Library module. Not directly callable.")

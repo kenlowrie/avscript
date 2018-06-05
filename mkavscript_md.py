5860ad0e88b604356f3f61c95bd7ad332a3db68c
Ken Lowrie <klowrie@gmail.com>
2018-06-04 22:09:36 -0500

    Code review changes - 1st round
    Use argparse for command line arguments. added -c, -d and -o, so now you can override the default CSS file, the default output path, and whether or not the app automatically opens the resultant output file with the system default app. Update shebang to use Python3. Only import what I need from system modules instead of everything. Initial PEP8 compliance changes.

--- mkavscript_md.py
+++ mkavscript_md.py
@@ -1,25 +1,28 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 """
-This module creates an HTML document suitable for printing of an
-AV script written in a specialized markdown that is processed by
-avscript_md.py.
+This module creates an HTML document suitable for printing.
+
+It uses avscript_md.py, which reads an AV script written in a specialized
+Markdown-syntax, and outputs HTML format. Since avscript_md is designed to
+be used by intermediate processors, it doesn't output the HTML header and
+footer tags; instead, it just writes everything between the <body></body>
+tags. mkavscript_md fills the gap by creating a properly formatted HTML
+document ready to be loaded into a browser (usually so you can save it as
+a PDF or <gasp> print it).
 
 usage: 
 
-    mkavscript_md avmarkdownfile
-    
-where:
+    mkavscript_md -h
 
-    avmarkdownfile - an av script written in avscript_md format
-              
-example:
-
-    mkavscript_md myavmdfile
+    mkavscript_md.py uses argparse, so just run it with -h for the syntax.
 """
 
-import os
-import sys
+from os import system, mkdir
+from os.path import split, splitext, join, isdir, isfile
+
+from sys import argv, exit
+
 import pylib
 
 def context(varfile=None):
@@ -28,44 +31,49 @@ def context(varfile=None):
     try:
         myself = __file__
     except NameError:
-        myself = sys.argv[0]
+        myself = argv[0]
 
     return pylib.context(myself,varfile)
 
 me = context('mkavscript_md')
 
-class C_FileName:
-    def __init__(self,mdfile):
-        path,name = os.path.split(mdfile)
-        file,ext = os.path.splitext(name)
+class FileName:
+    def __init__(self,mdfile,htmldir):
+        path,name = split(mdfile)
+        file,ext = splitext(name)
         
-        self.pathname = path
-        self.filename = name
-        self.rootname = file
-        self.file_ext = ext
+        self._pathname = path
+        self._filename = name
+        self._rootname = file
+        self._file_ext = ext
         
-        self.htmlfile = os.path.join(path,"html",file + ".html")
-        
-    def getPath(self):
-        return self.pathname
-        
-    def getFile(self):
-        return self.filename
-        
-    def getRootName(self):
-        return self.rootname
-        
-    def getExt(self):
-        return self.file_ext
-        
-    def getHTMLFile(self):
-        return self.htmlfile
+        self._htmlfile = join(path,htmldir,file + ".html")
+
+    @property
+    def path(self):
+        return self._pathname
+
+    @property
+    def filename(self):
+        return self._filename
+
+    @property
+    def rootname(self):
+        return self._rootname
+
+    @property
+    def extension(self):
+        return self._file_ext
+
+    @property
+    def HTML_filename(self):
+        return self._htmlfile
         
 
 
-def gethead(mdfile,cssFile):
-    path,name = os.path.split(mdfile)
-    file,ext = os.path.splitext(name)
+def get_head(mdfile,cssFile):
+    path,name = split(mdfile)
+    file,ext = splitext(name)
     
     head = """<!DOCTYPE html>
 <html lang="en">
@@ -81,68 +89,88 @@ def gethead(mdfile,cssFile):
 
     return head
     
-def gettail():
+def get_tail():
     return """
 </body>
 </html>"""
 
 def message(msgstr): print('{0}: {1}'.format(me.alias(),msgstr))
 
-def parse_args(args):
-
-    mdfile = ''    # file name to parse
-    
-    for arg in args:
-        if arg.lower() in ['--help', '-?', '-h', '/h', '/?']: usage()   # be nice, support command line help options
-        
-        mdfile = arg
-            
-    if not mdfile:   usage()
-    
-    return mdfile
-
-def addheader(fileobj,cssFile):
-    htmlfile = open(fileobj.getHTMLFile(), "w")
-    htmlfile.write(gethead(fileobj.getRootName(),cssFile))
+def add_header(fileobj,cssFile):
+    htmlfile = open(fileobj.HTML_filename, "w")
+    htmlfile.write(get_head(fileobj.rootname,cssFile))
     htmlfile.close()
 
-def addfooter(fileobj):
-    htmlfile = open(fileobj.getHTMLFile(), "a")
-    htmlfile.write(gettail())
+def add_footer(fileobj):
+    htmlfile = open(fileobj.HTML_filename, "a")
+    htmlfile.write(get_tail())
     htmlfile.close()
 
 
-def mkhtml(mdfile):
-    fileobj = C_FileName(mdfile)
-    cssFile = 'avscript_md.css'
-    cssSrcPath = os.path.join(me.whereami(),"avscript_md.css")
-    avscript_md = os.path.join(me.whereami(),"avscript_md.py")
-    if not os.path.isdir('./html'):
-        os.mkdir('./html')
-    os.system("cp \"{0}\" ./html/{1}".format(cssSrcPath,cssFile))
-    command = "{0} < \"{1}\" >> \"{2}\"".format(avscript_md,mdfile,fileobj.getHTMLFile())
-    message("Creating: " + fileobj.getRootName())
-    addheader(fileobj,cssFile)
-    os.system(command)
-    addfooter(fileobj)
-    os.system("open \"{0}\"".format(fileobj.getHTMLFile()))
+"""
+TODO:
+Need to make it so if you specify a path on cssfile, we strip it off since
+it will be copied to the destination folder. We only need the path when
+doing the transfer.
+
+outpath should also be handled better so that we can use relative or absolute
+and it will do the right thing with either.
+
+We need to open the file, and then modify these APIs to write to it, instead
+of using the system shell to do the work. i.e. make it pure Python.
+
+"""
+def _mkhtml(mdfile,cssfile, outpath, open_output_file):
+
+    fileobj = FileName(mdfile,outpath)
+    cssSrcPath = join(me.whereami(),cssfile)
+    if not isfile(cssSrcPath):
+        print("Can't find [{}] or [{}]. Giving up.".format(cssfile,cssSrcPath))
+        return 1
+
+    avscript_md = join(me.whereami(),"avscript_md.py")
+
+    if not isdir(outpath):
+        mkdir(outpath)
+    system("cp \"{0}\" {2}/{1}".format(cssSrcPath,cssfile,outpath))
+    command = "{0} < \"{1}\" >> \"{2}\"".format(avscript_md,mdfile,fileobj.HTML_filename)
+    message("Creating: " + fileobj.rootname)
+    add_header(fileobj,cssfile)
+    system(command)
+    add_footer(fileobj)
+    if(open_output_file):
+        system("open \"{0}\"".format(fileobj.HTML_filename))
 
     return 0
 
-
-
-def usage():
-    print __doc__   # just print out the modules' docstring
-    sys.exit(1)     # bail with non-zero exit code
+def mkavscript_md(arguments=None):
+    """Make an Audio-Visual Script from a text file written in a Markdown-like syntax.
     
+    Creates an HTML output file from the input file.
+    
+    if arguments is None, uses sys.argv - via argparse
+    if no filename is specified, parses sys.stdin -- TODO: IS THIS TRUE?
+    
+    Exit code:
+        0 -- Success
+        1 -- File not found
+        2 -- IO Error processing input file
+        3 -- Invalid regex ID in main loop
+    """
+    from argparse import ArgumentParser
+    
+    parser = ArgumentParser(description='Create an HTML file in Audio-Visual script format from a text file.', 
+                            epilog='If filename is not specified, program reads from stdin.')
+    parser.add_argument('-f', '--filename', required=True, help='the file that you want to parse')
+    parser.add_argument('-c', '--cssfile', nargs='?', const='avscript_md.css', default='avscript_md.css', help='the CSS file you want used for the styling. Default is avscript_md.css')
+    parser.add_argument('-d', '--path', nargs='?', const='./html', default='./html', help='the directory that you want the HTML file written to')
+    parser.add_argument('-o', '--open', nargs='?', const=True, default=False, help='whether or not to open the resulting HTML output file with the default system app')
+
+    args = parser.parse_args(None if arguments is None else arguments)
+    
+    return _mkhtml(args.filename, args.cssfile, args.path, args.open)
+
+
 if __name__ == '__main__':
-    # assume we have no arguments, but if we do, pass them along
-    args = []
-    if len(sys.argv) < 2: usage()
-    
-    args = sys.argv[1:]
-
-    mdfile = parse_args(args)
-    
-    sys.exit(mkhtml(mdfile))
+    exit(mkavscript_md())
     

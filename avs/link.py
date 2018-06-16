598c6fd98a163ba287d1516c6d090e5c71267a9d
Ken Lowrie <klowrie@gmail.com>
2018-06-16 09:25:45 -0500

    Added ability to encode mailto:
    To make it more challenging for spambots to mine email addresses out of the HTML documents generated.

--- avs/link.py
+++ avs/link.py
@@ -3,12 +3,65 @@
 from __future__ import print_function
 
 
+def str_to_html_entity_mix( string ):
+    """
+    Encodes a string using decimal or hexadecimal HTML character entities
+    
+    Encodes each character of the given string as either a decimal
+    or hexadecimal entity, in the hopes of foiling most email address
+    harvesting bots.
+    
+    Note that running the same string through this function produces
+    different results (intentionally). That's good, but it makes testing
+    a little more challenging.
+
+    Based on aea_encode_str() PHP function in Till Kruss's WordPress
+      Plugin; URI: http://wordpress.org/plugins/email-address-encoder/
+    Which is based on Michel Fortin's PHP Markdown:
+      http://michelf.com/projects/php-markdown/
+    Which is based on John Gruber's original Markdown:
+      http://daringfireball.net/projects/markdown/
+    Whose code is based on a filter by Matthew Wickline, posted to
+    the BBEdit-Talk with some optimizations by Milian Wolff.
+    """
+    chars = list(string)        # convert string to a list
+
+    from random import randint
+    from binascii import crc32
+    # generate a pseudo-random seed
+    seed = randint(0,int(abs(crc32(str.encode(string)) / len(string))))
+
+    # for each character in the string
+    for key in range(len(chars)):
+        char = chars[key]
+        ordc = ord(char)
+
+        # ascii only
+        if ordc < 128:
+
+            r = (seed * (1 + key)) % 100;
+
+            if(r > 75 and char not in ['@', '.', ':', ' ']):
+                # if r > 75 and char is not [@.: ], leave it intact
+                pass
+            elif (r < 42):
+                # r < 42 encode the character in hex format
+                chars[key] = '&#{};'.format(hex(ordc)[1:])
+            else:
+                # otherwise encode it as a decimal number
+                chars[key] = '&#{};'.format(ordc)
+
+    return ''.join(chars)   # return the new encoded string
+
 class _Link(object):
     """Class to abstract a reference link. Keep track of the URL (url) and
     the optional title (title). We don't track the name here, that is done
     in the C_Links class."""
     def __init__(self, url, title=None):
-        self.url = url.replace(" ","%20")
+        if(url[0:7] == 'mailto:'):
+            self.url = str_to_html_entity_mix(url.replace(" ","%20"))
+        else:
+            self.url = url.replace(" ","%20")
         self.title = title
 
 

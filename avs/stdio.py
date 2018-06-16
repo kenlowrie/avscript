faa2a8f3cc68640b282882cca00418bcb732b7c3
Ken Lowrie <klowrie@gmail.com>
2018-06-16 09:26:34 -0500

    Added ability to pass optional validation flag to setters
    When the unittest framework calls, we need to be able to tell the class to not validate that passed parameters are actual files. :)

--- avs/stdio.py
+++ avs/stdio.py
@@ -38,7 +38,7 @@ class StdioWrapper(object):
         return self._stdinput
 
     @stdinput.setter
-    def stdinput(self,new_input_file):
+    def stdinput(self,args):
         """
         Set the standard input stream to new_input_file
         
@@ -46,10 +46,16 @@ class StdioWrapper(object):
         file stack. The passed file must be open in 'r' or 'rb', and
         must be an instance of IOBase.
         """
-        if(not isinstance(new_input_file,IOBase)):
+        try:
+            new_input_file, validate = args
+        except ValueError:
+            new_input_file = args
+            validate = True
+        
+        if(validate and not isinstance(new_input_file,IOBase)):
             self._raise_wrong_type('new_input_file')
 
-        elif(new_input_file.mode not in ['r','rb']):
+        elif(validate and new_input_file.mode not in ['r','rb']):
             self._raise_wrong_mode(new_input_file.name,'r')
 
         self._stdinput.push(new_input_file)
@@ -65,11 +71,17 @@ class StdioWrapper(object):
         return self.stdoutput
 
     @stdoutput.setter
-    def stdoutput(self,new_output_file):
-        if(not isinstance(new_output_file, IOBase)):
-            self._throw_wrong_type('new_output_file')
+    def stdoutput(self,args):
+        try:
+            new_output_file, validate = args
+        except ValueError:
+            new_output_file = args
+            validate = True
+        
+        if(validate and not isinstance(new_output_file, IOBase)):
+            self._raise_wrong_type('new_output_file')
 
-        elif(new_output_file.mode not in ['w','wb']):
+        elif(validate and new_output_file.mode not in ['w','wb']):
             self._raise_wrong_mode(new_output_file.name,'w')
 
         self._stdoutput = new_output_file
@@ -85,11 +97,17 @@ class StdioWrapper(object):
         return self._stderror
 
     @stderror.setter
-    def stderror(self,new_error_file):
-        if(not isinstance(new_error_file,IOBase)):
-            self._throw_wrong_type('new_error_file')
+    def stderror(self,args):
+        try:
+            new_error_file, validate = args
+        except ValueError:
+            new_error_file = args
+            validate = True
+        
+        if(validate and not isinstance(new_error_file,IOBase)):
+            self._raise_wrong_type('new_error_file')
 
-        elif(new_error_file.mode not in ['w','wb']):
+        elif(validate and new_error_file.mode not in ['w','wb']):
             self._raise_wrong_mode(new_error_file.name,'w')
 
         self._stderror = new_error_file

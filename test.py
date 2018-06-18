b6b45e9c063b0bca7dc3a2b89a8eecdc30dee11f
Ken Lowrie <klowrie@gmail.com>
2018-06-17 22:35:17 -0500

    PEP8 Fixes identified by flake8

--- test.py
+++ test.py
@@ -6,27 +6,28 @@ from unittest import TestCase, TestLoader, TextTestRunner
 
 import avscript_md
 
+
 class TestAVScriptClass(TestCase):
     def setUp(self):
         self.avscript_md = avscript_md.AVScriptParser()
-        self.capturedOutput = io.StringIO()               # Create StringIO object
-        self.avscript_md.stdoutput = (self.capturedOutput, False)                   #  and redirect stdout.
+        self.capturedOutput = io.StringIO()     # Create StringIO object
+        self.avscript_md.stdoutput = (self.capturedOutput, False)   # and redirect stdout.
 
     def tearDown(self):
-        self.avscript_md.stdoutput = sys.__stdout__         # Reset redirect.
+        self.avscript_md.stdoutput = sys.__stdout__     # Reset redirect.
         del self.avscript_md
         self.avscript_md = None
         del self.capturedOutput
 
-    def process(self,which):
+    def process(self, which):
         self.avscript_md.open_and_parse("tests/in/{}.md".format(which))
         with open('tests/out/{}.html'.format(which), 'r') as myfile:
             data = myfile.read()
         with open('tests/run/{}.out'.format(which), 'w') as mf2:
             mf2.write(self.capturedOutput.getvalue())
 
-        self.assertEqual(self.capturedOutput.getvalue(),data)
-        
+        self.assertEqual(self.capturedOutput.getvalue(), data)
+
     def test_markdown(self):
         self.process('markdown')
 

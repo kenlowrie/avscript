17878ae678efc2d6fdc1399db1efa6815ddfef06
Ken Lowrie <klowrie@gmail.com>
2018-09-07 18:36:14 -0500

    Added split_as to split the public_attrs string
    Then ended up going a different way. Leaving it for now...

--- avscript/avs/utility.py
+++ avscript/avs/utility.py
@@ -16,5 +16,17 @@ class CodeHelpers():
     def b(i):
         return '{{' if i == 0 else '}}'
 
+    @staticmethod
+    def split_as(s):
+        from .regex import Regex
+        r = Regex(r'\s?([\w]+)\s*=\s*\"(.*?)(?<!\\)\"')
+
+        d = {l[0]: l[1] for l in r.regex.findall(s)}
+        news = ''
+        for i in d:
+            news += '***{}***={}<br />'.format(i, HtmlUtils().escape_html(d[i]))
+
+        return news
+
 if __name__ == '__main__':
     print("Library module. Not directly callable.")

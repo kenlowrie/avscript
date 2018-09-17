e4d58a46532ce081f5ddc7dabc1d6613de044919
Ken Lowrie <klowrie@gmail.com>
2018-09-17 07:23:15 -0500

    Refactored the debug interfaces
    All output now routes through a centralized printer, so we can suspend/resume output on the fly. Messages printed while debug is suspended are cached, and then dumped when its resumed.

--- avscript/avs/debug.py
+++ avscript/avs/debug.py
@@ -1,25 +1,48 @@
 #!/usr/bin/env python
 
-def default_debug_register(obj, dups_okay):
+def default_debug_register(obj):
+    # A default register in case someone tries to instantiate a Debug() object
+    # before a DebugTracker() is up and running and the registration API exposed
     raise NotImplementedError("You need to define a default debug register first")
 
+# When a Debug() object is instantiated, it registers itself with the DebugTracker
+# via this interface.
 gb_debug_register_tag = default_debug_register
+# The suspend interface is initialized when the debug_register is done. This API
+# provides a way for any Debug() instance to suspend/resume output.
+gb_debug_suspend_xface = None
 
 def set_default_debug_register(func):
     if not isinstance(func, DebugTracker):
         raise NameError("set_default_debug_register requires a DebugTracker object")
 
-    global gb_debug_register_tag
+    global gb_debug_register_tag, gb_debug_suspend_xface
     gb_debug_register_tag = func.debug_register_xface
+    gb_debug_suspend_xface = func.debug_suspend_xface
+
+def default_debug_print(s):
+    # We have a default handler for printing in case we are called too early...
+    raise NotImplementedError("You need to define a default debug print handler")
+
+# This is the API that prints debug messages. Debug() objects call their print() method
+# Which uses this entry point to serialize the messages through a common output function,
+# enabling us to control output globally.
+
+gb_debug_print = default_debug_print
 
 
 class Debug(object):
-    def __init__(self, classtag, initial_state=False, output=None):
+    def __init__(self, classtag, initial_state=False):
         self._state = initial_state
         self._tag = '{}.{}'.format(classtag,id(self))
-        self._out = output if output is not None else print
         global gb_debug_register_tag
         gb_debug_register_tag(self)
+
+    def global_suspend(self, suspend):
+        # used to globally suspend debug messages
+        global gb_debug_suspend_xface
+        gb_debug_suspend_xface(suspend)
+
 
     @property
     def state(self):
@@ -53,20 +76,45 @@ class Debug(object):
     def enabled(self):
         return self.state
 
+    def print_msg(self, msg, context=None):
+        s = '{:>20}: {}{}<br />\n'.format(self._tag, '{}'.format('' if context is None else '({})'.format(context)), msg)
+        global gb_debug_print
+        gb_debug_print('<span class="debug">{}</span>'.format(s))
+
     def print(self, msg, context=None):
         if not self.state:
             return
 
-        s = '{:>20}: {}{}<br />\n'.format(self._tag, '{}'.format('' if context is None else '({})'.format(context)), msg)
-        self._out('<span class="debug">{}</span>'.format(s))
-
+        self.print_msg(msg, context)
 
 class DebugTracker(object):
     def __init__(self, output=None):
+        self.suspendCounter=0
         self._debug_tags = {}
-        self._out = output if output is not None else print
+        self._msg_cache = []
         global gb_debug_register_tag
         gb_debug_register_tag = self.debug_register_xface
+
+        global gb_debug_print
+        gb_debug_print = self.print
+
+        # This is the output method. print() by default, unless passed in
+        self._out = output
+        
+        self.sys_debug = Debug('_SYSTEM')    # Define our own tracker to format...
+
+    def print(self, s):
+        if self.suspendCounter == 0:
+            self._out(s)
+        else:
+            self._msg_cache.append(s)
+
+    def output(self, s):
+        # We only want to print if at least one debug tag is on
+        # This prevents spurious messages from being output when
+        # debug tracking is not currently enabled.
+        if self.debug_active():
+            self.sys_debug.print_msg(s)
 
     @property
     def debug_tags(self):
@@ -95,6 +143,30 @@ class DebugTracker(object):
 
         return self.debug_tags[tag].state
 
+    def dump_cache(self):
+        msgs = self._msg_cache[::-1]
+        self._msg_cache = []
+        if len(msgs):
+            self.output("Dumping debug cache.")
+            while len(msgs):
+                self._out(msgs.pop())
+
+    def debug_suspend_xface(self, suspend):
+        if suspend == True:
+            self.output("Debug output has been suspended.")
+            self.suspendCounter += 1
+        else:
+            self.suspendCounter -= 1
+            if self.suspendCounter < 0:
+                raise ValueError("Debug suspend counter went below zero.")
+            elif self.suspendCounter == 0:
+                # Dump any cached messages
+                self.dump_cache()
+                self.output("Debug output will be resumed.")
+            else:
+                # cache a debug output decrement counter message
+                self.output("Debug output suspendCounter--")
+
     def debug_register_xface(self, obj):
         if not isinstance(obj, Debug):
             raise NameError("obj must be instance of Debug class")
@@ -103,6 +175,14 @@ class DebugTracker(object):
             raise NameError("tag {} already registered".format(obj.tag))
 
         self.debug_tags[obj._tag] = obj
+
+    def debug_active(self):
+        # Check to see if any debug trackers are enabled
+        for var in self.debug_tags:
+            if self.debug_tags[var].enabled():
+                return True
+
+        return False
 
     def call(self, which, method):
         from .regex import RegexSafe
@@ -129,7 +209,6 @@ class DebugTracker(object):
     def enabled(self, tag):
         self.call(tag, 'enabled')
 
-    #TODO: Should I have a print that takes a wildcard? print('avscript.header', 'msg'...)
     def dumpTags(self):
         for tag in self.debug_tags:
             self._out("{} is {}<br />".format(tag, 'on' if self.debug_tags[tag].state else 'off'))
